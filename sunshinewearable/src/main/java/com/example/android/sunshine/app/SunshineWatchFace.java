4d9d2ce7a02c5d6d03261480e06e1eab596995d1
Brett <bretttucker@gmail.com>
2016-03-13 18:03:19 -0600

    Fix typo

--- sunshinewearable/src/main/java/com/example/android/sunshine/app/SunshineWatchFace.java
+++ sunshinewearable/src/main/java/com/example/android/sunshine/app/SunshineWatchFace.java
@@ -204,7 +204,7 @@ public class SunshineWatchFace extends CanvasWatchFaceService {
         }
 
         private GoogleApiClient getGoogleApiClient(){
-            return new GoogleApiClient.Builder(this)
+            return new GoogleApiClient.Builder(getApplicationContext())
                     .addApi(Wearable.API)
                     .build();
         }
